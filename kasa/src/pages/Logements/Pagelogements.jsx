b0c43a15efa77cb92c5007d4cad38943fec4dd0a
SOUSOUBENH <75803460+sousouben@users.noreply.github.com>
2022-10-31 11:13:54 +0100

    correction erreur

--- kasa/src/pages/Logements/Pagelogements.jsx
+++ kasa/src/pages/Logements/Pagelogements.jsx
@@ -4,30 +4,32 @@ import interLogement from "../../assets/images/interieur.png";
 import Carroussel from "../../Components/Carroussel/Carroussel";
 
 function Pagelogements() {
-  <div className="logementSelectionner">
-    <div className="banniere-logement">
-      <Carroussel />
-      <img src={interLogement} alt="logement séléctionné" />
-    </div>
-    <div className="titreLogement">
-      <div className="titre-nom">
-        <h2>titre</h2>
-        <p>sous-titre</p>
+  return (
+    <div className="logementSelectionner">
+      <div className="banniere-logement">
+        <Carroussel />
+        <img src={interLogement} alt="logement séléctionné" />
+      </div>
+      <div className="titreLogement">
+        <div className="titre-nom">
+          <h2>titre</h2>
+          <p>sous-titre</p>
+        </div>
+      </div>
+      <div className="nomLogement">
+        <h3>Nom</h3>
+        <div className="avatar"></div>
+      </div>
+      <div className="tag-evaluation">
+        <div className="tags"></div>
+        <div className="nbre-etoile"></div>
+      </div>
+      <div className="descrpt-epuip">
+        <div className="descrption"></div>
+        <div className="equipements"></div>
       </div>
     </div>
-    <div className="nomLogement">
-      <h3>Nom</h3>
-      <div className="avatar"></div>
-    </div>
-    <div className="tag-evaluation">
-      <div className="tags"></div>
-      <div className="nbre-etoile"></div>
-    </div>
-    <div className="descrpt-epuip">
-      <div className="descrption"></div>
-      <div className="equipements"></div>
-    </div>
-  </div>;
+  );
 }
 
 export default Pagelogements;
