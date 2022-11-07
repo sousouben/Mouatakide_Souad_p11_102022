ee7569b8d16d04e9cf053ca914ae834acbdaa992
SOUSOUBENH <75803460+sousouben@users.noreply.github.com>
2022-11-07 12:28:55 +0100

    finition de la page À propos
    Ajout des collapses

--- kasa/src/Components/Dropdown/Dropdown.jsx
+++ kasa/src/Components/Dropdown/Dropdown.jsx
@@ -1,9 +1,46 @@
 import React from "react";
+import { useState } from "react";
+import close from "../../assets/icones/fermer.png";
+import open from "../../assets/icones/ouvert.png";
 import "./Dropdown.css";
-//import fermer from "../../assets/icones/fermer.png";
-//import ouvert from "../../assets/icones/ouvert.png";
-const Dropdown = () => {
-  return <div></div>;
-};
+
+function Dropdown({ title, text, extraClass }) {
+  const [dropdownOpen, setDropdown] = useState(false);
+
+  return dropdownOpen ? (
+    <div className={extraClass ? "dropdown " + extraClass : "dropdown"}>
+      <div className="titre-dropdown">
+        <h3>{title}</h3>
+        <img
+          src={close}
+          alt="Contenu invisible"
+          onClick={() => setDropdown(false)}
+        />
+      </div>
+      {typeof text === "object" ? tabEquipement(text) : <p>{text}</p>}
+    </div>
+  ) : (
+    <div className="dropdown">
+      <div className="titre-dropdown">
+        <h3>{title}</h3>
+        <img
+          src={open}
+          alt="Contenu visible"
+          onClick={() => setDropdown(true)}
+        />
+      </div>
+    </div>
+  );
+}
+
+function tabEquipement(tab) {
+  return (
+    <ul>
+      {tab.map((equipment) => (
+        <li>{equipment}</li>
+      ))}
+    </ul>
+  );
+}
 
 export default Dropdown;
