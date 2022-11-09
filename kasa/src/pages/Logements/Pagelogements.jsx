bb8a1a02031dda24c5c0b4c1edabd56fbe93ae08
SOUSOUBENH <75803460+sousouben@users.noreply.github.com>
2022-11-09 11:15:04 +0100

    ajout tag dans dossier pagelogement

--- kasa/src/pages/Logements/Pagelogements.jsx
+++ kasa/src/pages/Logements/Pagelogements.jsx
@@ -3,16 +3,14 @@ import "./Pagelogements.css";
 import { getId } from "../../data/services";
 import { useParams } from "react-router-dom";
 import Carroussel from "../../Components/Carroussel/Carroussel";
-import Page404 from "../Erreur/Page404";
 import Dropdown from "../../Components/Dropdown/Dropdown";
+import Tag from "../../Components/Tags/Tags";
 
 function Pagelogements() {
   const { logementId } = useParams();
   const ficheLogement = getId(logementId);
 
-  return Pagelogements === undefined ? (
-    <Page404 />
-  ) : (
+  return (
     <div className="ficheLogement">
       <div className="navig">
         <Carroussel pictures={ficheLogement.pictures} />
@@ -24,13 +22,20 @@ function Pagelogements() {
         </div>
         <div className="nomLogement">
           <h3>{ficheLogement.host.name}</h3>
-          <img src={ficheLogement.host.picture} alt="" />
+          <img
+            src={ficheLogement.host.picture}
+            alt={ficheLogement.host.picture}
+          />
         </div>
       </div>
 
       <div className="tag-evaluation">
-        <div className="tags">{ficheLogement.tags}</div>
-        <div className="start"></div>
+        <div className="tags">
+          {ficheLogement.tags.map((tag) => (
+            <Tag tagsNom={tag} key={tag} />
+          ))}
+        </div>
+        <div className="star"></div>
       </div>
       <div className="descript-epuip">
         <div className="description">
